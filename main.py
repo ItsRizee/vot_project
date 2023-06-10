2563233f27154e8d86b9db6663823af54fb8e39c
ItsRizee <101361202+ItsRizee@users.noreply.github.com>
2023-06-10 19:47:23 +0300

    Finished working on contact.html page

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        # Process the form data (e.g., send an email, store in a database, etc.)
+
+        # Return a response or redirect to a thank you page
+        return 'Thank you for your message!'
+
+    return render_template('contact.html')
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run()
