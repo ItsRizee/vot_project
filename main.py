812aa0ac98486f3f5569a584b7c6063b776c187c
ItsRizee <101361202+ItsRizee@users.noreply.github.com>
2023-06-11 11:37:32 +0300

    Finished working on new_journey.html page

--- main.py
+++ main.py
@@ -2,31 +2,36 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return render_template('home.html')
 
+@app.route('/new_journey', methods=['POST', 'GET'])
+def new_journey():
+    if request.method == 'POST':
+        # Process the form data
+        title = request.form['title']
+        country = request.form['country']
+        description = request.form['description']
+        images = request.files.getlist('image')
 
-@app.route('/contact', methods=['GET', 'POST'])
+
+    return render_template('new_journey.html')
+
+@app.route('/contact', methods=['POST', 'GET'])
 def contact():
     if request.method == 'POST':
+        # Process the form data
         name = request.form['name']
         email = request.form['email']
         message = request.form['message']
 
-        # Process the form data (e.g., send an email, store in a database, etc.)
-
-        # Return a response or redirect to a thank you page
-        return 'Thank you for your message!'
 
     return render_template('contact.html')
-
 
 @app.route('/about')
 def about():
     return render_template('about.html')
-
 
 if __name__ == '__main__':
     app.run()
